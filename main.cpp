84f3087fb161bfd16208bb11495024c0c5c2532c
TuAmor <2021256010@ub.edu.bz>
2025-10-01 20:03:59 -0600

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+// Filename: main.cpp
+// Each node will store a string as its data
+struct BinaryTreeNode {
+    string data;
+    BinaryTreeNode* left;
+    BinaryTreeNode* right;
+
+    BinaryTreeNode(const string& value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+
+int main()
+{
+	return 0;
+{
+
+
+
+
+void inorder(BinaryTreeNode* root) {
+    if (root == nullptr){
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
